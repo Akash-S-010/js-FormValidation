d6ffdf684edb43d6a231b429c4e01202c03a906b
Akash <akashspalloor@gmail.com>
2024-10-25 22:40:24 +0530

    js competed

---
+++ script.js
@@ -0,0 +1,76 @@
+const form = document.querySelector('#form')
+let userName = document.querySelector("#username")
+let email = document.querySelector("#email")
+let password = document.querySelector("#password")
+let password2 = document.querySelector("#password2");
+let unameError = document.querySelector(".username-error-msg");
+let emailError = document.querySelector(".email-error-msg");
+let passwordError = document.querySelector(".password-error-msg");
+let password2Error = document.querySelector(".password2-error-msg")
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault() 
+
+    // Username checking
+    if (userName.value === "") {
+        unameError.innerHTML = "Username Cannot be Empty"
+        showError(userName)
+    } else {
+        unameError.innerHTML = ""
+        showSuccess(userName)
+    }
+
+    // Email Checking
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    if (email.value === "") {
+        emailError.innerHTML = "invalid Email"
+        showError(email)
+        
+    } else if (!emailPattern.test(email.value)) {
+        emailError.innerHTML = "Invalid Email"
+        showError(email)
+
+    } else {
+        emailError.innerHTML = "" 
+        showSuccess(email)
+    }
+
+    // Password Checking
+    if (password.value === "") {
+        passwordError.innerHTML = "Password Cannot be Empty"
+        showError(password)
+
+    } else if (password.value.length < 6) { 
+        passwordError.innerHTML = "Minimum six characters required"
+        showError(password)
+
+    } else {
+        passwordError.innerHTML = ""
+        showSuccess(password)
+    }
+
+    // Password2 Checking
+    if (password2.value === "") {
+        password2Error.innerHTML = "Password Cannot be Empty" 
+        showError(password2)
+
+    } else if (password2.value !== password.value) {
+        password2Error.innerHTML = "Passwords do not match" 
+        showError(password2)
+
+    } else {
+        password2Error.innerHTML = ""
+        showSuccess(password2)
+    }
+})
+
+
+function showError(input) {
+    input.parentElement.classList.add("error")
+    input.parentElement.classList.remove("success")
+}
+
+function showSuccess(input) {
+    input.parentElement.classList.add("success")
+    input.parentElement.classList.remove("error")
+}
