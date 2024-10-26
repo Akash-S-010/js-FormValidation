9a71926e9ecc03ef956f69bcc9e4fe6c6fb7f45e
Akash S <akashspalloor@gmail.com>
2024-10-26 22:18:08 +0530

    make it simple by passing error messgaes also in the Showerror(passed the error message as a parameter),ShowSuccess functions

--- script.js
+++ script.js
@@ -9,69 +9,65 @@ let passwordError = document.querySelector(".password-error-msg");
 let password2Error = document.querySelector(".password2-error-msg")
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault() 
+    e.preventDefault()
 
     // Username checking
     if (userName.value === "") {
-        unameError.innerHTML = "Username Cannot be Empty"
-        showError(userName)
+        showError(userName, "Username Cannot be Empty")
+
+    } else if (userName.value.length < 4 || userName.value.length > 12) {
+        showError(userName, "Username must be 4 - 12 characters.")
+
     } else {
-        unameError.innerHTML = ""
         showSuccess(userName)
     }
 
     // Email Checking
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-    if (email.value === "") {
-        emailError.innerHTML = "invalid Email"
-        showError(email)
-        
+    if (email.value == "") {
+        showError(email, "Email Cannot be Empty")
+
     } else if (!email.value.match(emailPattern)) {
-        emailError.innerHTML = "Invalid Email"
-        showError(email)
+        showError(email, "Invalid Email")
 
     } else {
-        emailError.innerHTML = "" 
         showSuccess(email)
     }
 
     // Password Checking
     if (password.value === "") {
-        passwordError.innerHTML = "Password Cannot be Empty"
-        showError(password)
+        showError(password, "Password Cannot be Empty")
 
-    } else if (password.value.length < 6) { 
-        passwordError.innerHTML = "Minimum six characters required"
-        showError(password)
+    } else if (password.value.length < 6) {
+        showError(password, "Minimum six characters required")
 
     } else {
-        passwordError.innerHTML = ""
         showSuccess(password)
     }
 
     // Password2 Checking
     if (password2.value === "") {
-        password2Error.innerHTML = "Password Cannot be Empty" 
-        showError(password2)
+        showError(password2, "Password Cannot be Empty")
 
     } else if (password2.value !== password.value) {
-        password2Error.innerHTML = "Passwords do not match" 
-        showError(password2)
+        showError(password2, "Passwords do not match")
 
     } else {
-        password2Error.innerHTML = ""
         showSuccess(password2)
     }
 })
 
-// Function to show error (add 'error' class and remove 'success')
-function showError(input) {
+// Function to show error 
+function showError(input, errorMsg) {
     input.parentElement.classList.add("error")
     input.parentElement.classList.remove("success")
+    input.parentElement.querySelector('.error-para').innerHTML = errorMsg;
+
 }
 
-// Function to show success (add 'success' class and remove 'error')
+// Function to show success 
 function showSuccess(input) {
     input.parentElement.classList.add("success")
     input.parentElement.classList.remove("error")
+    input.parentElement.querySelector('.error-para').innerHTML = ""
 }
